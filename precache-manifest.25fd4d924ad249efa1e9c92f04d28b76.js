b8de908c694529de5d13f44b6cc508f8a2a99428
Yogesh vishnole <yogeshvishnole@gmail.com>
2020-03-21 19:57:57 +0530

    Updates

---
+++ precache-manifest.25fd4d924ad249efa1e9c92f04d28b76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acde278c67e53c4855a17f9d02a9c3b1",
+    "url": "/yogesh-monsters/index.html"
+  },
+  {
+    "revision": "a3cef511608af270a815",
+    "url": "/yogesh-monsters/static/css/main.62d87302.chunk.css"
+  },
+  {
+    "revision": "f6d2ab47bb337dc705a8",
+    "url": "/yogesh-monsters/static/js/2.b67fbdca.chunk.js"
+  },
+  {
+    "revision": "a3cef511608af270a815",
+    "url": "/yogesh-monsters/static/js/main.bf66b918.chunk.js"
+  },
+  {
+    "revision": "60d028679323879c7f54",
+    "url": "/yogesh-monsters/static/js/runtime~main.4d959f97.js"
+  }
+]);
